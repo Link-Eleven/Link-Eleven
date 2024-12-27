cc6aa79f3dfaca7a37f9dc96d1b22cd688bd84c4
jshstar <jshstar97@gmail.com>
2024-12-27 18:42:47 +0900

    [ init ] Coupon Init

---
+++ coupon/src/main/java/com/linkeleven/msa/coupon/CouponApplication.java
@@ -0,0 +1,15 @@
+package com.linkeleven.msa.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class CouponApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponApplication.class, args);
+	}
+
+}

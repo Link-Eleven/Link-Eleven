2c120a094a3d1c6d025f7fc845549f3ff43de7ea
jshstar <jshstar97@gmail.com>
2024-12-27 18:48:24 +0900

    [ init ] Notification Init

---
+++ notification/src/test/java/com/linkeleven/msa/notification/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkeleven.msa.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

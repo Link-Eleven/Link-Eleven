2d6e2a5ac09a8d7f2ad840e5ee0ee11bba0499da
jshstar <jshstar97@gmail.com>
2024-12-29 14:38:17 +0900

    [ feat ] 컨트롤러 Feign Client 테스트 구현 ( #2 )

---
+++ area/src/main/java/com/linkeleven/msa/area/presentation/controller/external/AreaExternalController.java
@@ -0,0 +1,52 @@
+package com.linkeleven.msa.area.presentation.controller.external;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.linkeleven.msa.area.application.dto.external.AdministrativeRegionDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/external")
+public class AreaExternalController {
+
+
+	// 내가 해당하는 지역의 행정구역
+	@GetMapping("/location")
+	public AdministrativeRegionDto findAdministrativeRegion(
+		@RequestParam double latitude,
+		@RequestParam double longitude
+	){
+
+		return AdministrativeRegionDto.builder()
+			.regionName("서울특별시")
+			.build();
+	}
+
+	// 해당 주소 입력의 지역 행정구역
+	@GetMapping("/location/{address}")
+	public AdministrativeRegionDto findAddressRegion(
+		@PathVariable String address
+	){
+		return AdministrativeRegionDto.builder()
+			.regionName("서울특별시")
+			.build();
+	}
+
+	// 해당 키워드 입력의 지역 행정구역
+	@GetMapping("/location/keyword")
+	public AdministrativeRegionDto findKeywordByRegion(
+		@RequestParam String keyword
+	){
+		return AdministrativeRegionDto.builder()
+			.regionName("서울특별시")
+			.build();
+	}
+
+
+}

fc268bc7508bb4ad562492db5592fbc2cb1ee3f1
jshstar <jshstar97@gmail.com>
2024-12-29 14:38:31 +0900

    [ feat ] 컨트롤러 Feign Client 테스크 코드 구현 ( #2 )

---
+++ area/src/test/java/com/linkeleven/msa/area/presentation/controller/external/AreaExternalControllerTest.java
@@ -0,0 +1,42 @@
+package com.linkeleven.msa.area.presentation.controller.external;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AreaExternalControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	void testFindAdministrativeRegion() throws Exception {
+		mockMvc.perform(get("/external/location")
+				.param("latitude", "37.5665")
+				.param("longitude", "126.9780"))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.regionName").value("서울특별시"));
+	}
+
+	@Test
+	void testFindAddressRegion() throws Exception {
+		mockMvc.perform(get("/external/location/{address}", "서울특별시 강남구"))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.regionName").value("서울특별시"));
+	}
+
+	@Test
+	void testFindKeywordByRegion() throws Exception {
+		mockMvc.perform(get("/external/location/keyword")
+				.param("keyword", "강남"))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.regionName").value("서울특별시"));
+	}
+}

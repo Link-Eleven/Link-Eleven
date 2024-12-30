a148b34da8c167878fb79131f8834acf845eeb0f
jshstar <jshstar97@gmail.com>
2024-12-30 14:44:50 +0900

    [ fix ] Dto 수정 ( #2 )

--- area/src/main/java/com/linkeleven/msa/area/presentation/controller/external/AreaExternalController.java
+++ area/src/main/java/com/linkeleven/msa/area/presentation/controller/external/AreaExternalController.java
@@ -11,39 +11,39 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/external/area")
+@RequestMapping("/external/areas")
 public class AreaExternalController {
 
 
 	// 내가 해당하는 지역의 행정구역
-	@GetMapping("/location/coordinate")
+	@GetMapping("/locations/coordinates")
 	public AdministrativeRegionDto findAdministrativeRegion(
 		@RequestParam double latitude,
 		@RequestParam double longitude
 	){
 
 		return AdministrativeRegionDto.builder()
-			.regionName("서울특별시")
+			.areaId(556028620837202474L)
 			.build();
 	}
 
 	// 해당 주소 입력의 지역 행정구역
-	@GetMapping("/location/address")
+	@GetMapping("/locations/address")
 	public AdministrativeRegionDto findAddressRegion(
 		@RequestParam String address
 	){
 		return AdministrativeRegionDto.builder()
-			.regionName("서울특별시")
+			.areaId(556028620837202474L)
 			.build();
 	}
 
 	// 해당 키워드 입력의 지역 행정구역
-	@GetMapping("/location/keyword")
+	@GetMapping("/locations/keywords")
 	public AdministrativeRegionDto findKeywordByRegion(
 		@RequestParam String keyword
 	){
 		return AdministrativeRegionDto.builder()
-			.regionName("서울특별시")
+			.areaId(556028620837202474L)
 			.build();
 	}
 

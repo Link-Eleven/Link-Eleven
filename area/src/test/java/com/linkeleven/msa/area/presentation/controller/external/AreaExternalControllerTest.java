b8c59c48ef11c8a15225852fa2857f4ea48788ac
jshstar <jshstar97@gmail.com>
2024-12-30 16:38:30 +0900

    [ Test ] 테스트 코드 수정 ( #2 )

--- area/src/test/java/com/linkeleven/msa/area/presentation/controller/external/AreaExternalControllerTest.java
+++ area/src/test/java/com/linkeleven/msa/area/presentation/controller/external/AreaExternalControllerTest.java
@@ -22,7 +22,7 @@ class AreaExternalControllerTest {
 				.param("latitude", "37.5665")
 				.param("longitude", "126.9780"))
 			.andExpect(status().isOk())
-			.andExpect(jsonPath("$.regionName").value(556028620837202474L));
+			.andExpect(jsonPath("$.areaId").value(556028620837202474L));
 	}
 
 	@Test
@@ -30,14 +30,14 @@ class AreaExternalControllerTest {
 		mockMvc.perform(get("/external/areas/locations/address")
 				.param("address", "서울특별시 강남구 강남대로 390"))
 			.andExpect(status().isOk())
-			.andExpect(jsonPath("$.regionName").value(556028620837202474L));
+			.andExpect(jsonPath("$.areaId").value(556028620837202474L));
 	}
 
 	@Test
 	void testFindKeywordByRegion() throws Exception {
-		mockMvc.perform(get("/external/areas/locations/keyword")
+		mockMvc.perform(get("/external/areas/locations/keywords")
 				.param("keyword", "강남"))
 			.andExpect(status().isOk())
-			.andExpect(jsonPath("$.regionName").value(556028620837202474L));
+			.andExpect(jsonPath("$.areaId").value(556028620837202474L));
 	}
 }

f246f99b4cd84c02d5885fa662acdf98d57366c8
jeendale <jeekoo7203@gmail.com>
2024-12-31 02:34:55 +0900

    [ feat ] User domain 구현 ( #12 )

---
+++ auth/src/main/java/com/linkeleven/msa/auth/domain/model/User.java
@@ -0,0 +1,47 @@
+package com.linkeleven.msa.auth.domain.model;
+
+import com.linkeleven.msa.auth.domain.common.UserRole;
+
+import io.hypersistence.utils.hibernate.id.Tsid;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name="p_user")
+public class User extends BaseTime {
+	@Id
+	@Tsid
+	private Long userId;
+
+	@Column(nullable = false)
+	private String username;
+
+	@Column(nullable = false)
+	private String password;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private UserRole role;
+
+	@Column(nullable = false)
+	private boolean isAnonymous;
+
+	@Column
+	private boolean isCouponIssued;
+
+
+
+}

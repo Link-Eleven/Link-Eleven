3d2c6f83739b962bfbdd3da42779b3ec572c8733
chc <311mikecho@gmail.com>
2024-12-31 16:39:50 +0900

    [ refactor ] 게시글 생성 dto 분리 #14

--- feed/src/main/java/com/linkeleven/msa/feed/application/service/FeedService.java
+++ feed/src/main/java/com/linkeleven/msa/feed/application/service/FeedService.java
@@ -3,11 +3,13 @@ package com.linkeleven.msa.feed.application.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.linkeleven.msa.feed.application.dto.FeedCreateResponseDto;
 import com.linkeleven.msa.feed.application.dto.FeedUpdateResponseDto;
 import com.linkeleven.msa.feed.domain.model.Feed;
 import com.linkeleven.msa.feed.domain.repository.FeedRepository;
 import com.linkeleven.msa.feed.libs.exception.CustomException;
 import com.linkeleven.msa.feed.libs.exception.ErrorCode;
+import com.linkeleven.msa.feed.presentation.request.FeedCreateRequestDto;
 import com.linkeleven.msa.feed.presentation.request.FeedUpdateRequestDto;
 
 import lombok.RequiredArgsConstructor;
@@ -18,18 +20,18 @@ public class FeedService {
 
 	private final FeedRepository feedRepository;
 
-	// @Transactional
-	// public FeedCreateResponseDto createFeed(FeedCreateRequestDto feedCreateRequestDto) {
-	// 	Feed feed = Feed.of(
-	// 		feedCreateRequestDto.getUserId(),
-	// 		feedCreateRequestDto.getLocationId(),
-	// 		feedCreateRequestDto.getTitle(),
-	// 		feedCreateRequestDto.getContent(),
-	// 		feedCreateRequestDto.getCategory()
-	// 	);
-	// 	Feed savedFeed = feedRepository.save(feed);
-	// 	return FeedCreateResponseDto.from(savedFeed);
-	// }
+	@Transactional
+	public FeedCreateResponseDto createFeed(FeedCreateRequestDto feedCreateRequestDto) {
+		Feed feed = Feed.of(
+			feedCreateRequestDto.getUserId(),
+			feedCreateRequestDto.getLocationId(),
+			feedCreateRequestDto.getTitle(),
+			feedCreateRequestDto.getContent(),
+			feedCreateRequestDto.getCategory()
+		);
+		Feed savedFeed = feedRepository.save(feed);
+		return FeedCreateResponseDto.from(savedFeed);
+	}
 
 	@Transactional
 	public FeedUpdateResponseDto updateFeed(FeedUpdateRequestDto feedUpdateRequestDto) {
--- feed/src/main/java/com/linkeleven/msa/feed/presentation/controller/FeedController.java
+++ feed/src/main/java/com/linkeleven/msa/feed/presentation/controller/FeedController.java
@@ -6,16 +6,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.linkeleven.msa.feed.application.dto.FeedCreateResponseDto;
 import com.linkeleven.msa.feed.application.dto.FeedReadResponseDto;
 import com.linkeleven.msa.feed.application.dto.FeedUpdateResponseDto;
 import com.linkeleven.msa.feed.application.service.FeedService;
 import com.linkeleven.msa.feed.libs.dto.SuccessResponseDto;
+import com.linkeleven.msa.feed.presentation.request.FeedCreateRequestDto;
 import com.linkeleven.msa.feed.presentation.request.FeedUpdateRequestDto;
 
 import lombok.RequiredArgsConstructor;
@@ -27,13 +30,13 @@ public class FeedController {
 
 	private final FeedService feedService;
 
-	// @PostMapping
-	// public ResponseEntity<SuccessResponseDto<FeedCreateResponseDto>> createFeed(
-	// 	@RequestBody FeedCreateRequestDto feedCreateRequestDto) {
-	//
-	// 	FeedCreateResponseDto response = feedService.createFeed(feedCreateRequestDto);
-	// 	return ResponseEntity.ok(SuccessResponseDto.success("게시글 생성 성공", response));
-	// }
+	@PostMapping
+	public ResponseEntity<SuccessResponseDto<FeedCreateResponseDto>> createFeed(
+		@RequestBody FeedCreateRequestDto feedCreateRequestDto) {
+
+		FeedCreateResponseDto response = feedService.createFeed(feedCreateRequestDto);
+		return ResponseEntity.ok(SuccessResponseDto.success("게시글 생성 성공", response));
+	}
 
 	@PutMapping("/{feedId}")
 	public ResponseEntity<SuccessResponseDto<FeedUpdateResponseDto>> updateFeed(

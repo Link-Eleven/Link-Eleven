a1507755244e58e23b9bd95242bd0a07178071b9
jeendale <jeekoo7203@gmail.com>
2024-12-31 12:01:01 +0900

    [ refactor ] custom filter Slf4j추가 ( #7 )

--- gateway/src/main/java/com/linkeleven/msa/gateway/CustomPostFilter.java
+++ gateway/src/main/java/com/linkeleven/msa/gateway/CustomPostFilter.java
@@ -1,23 +1,23 @@
 package com.linkeleven.msa.gateway;
 
-import java.util.logging.Logger;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
+
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
 @Component
+@Slf4j
 public class CustomPostFilter implements GlobalFilter, Ordered {
-
-  private static final Logger logger = Logger.getLogger(CustomPostFilter.class.getName());
 
   @Override
   public Mono<Void> filter(ServerWebExchange exchange, org.springframework.cloud.gateway.filter.GatewayFilterChain chain) {
     return chain.filter(exchange).then(Mono.fromRunnable(() -> {
       ServerHttpResponse response = exchange.getResponse();
-      logger.info("Post Filter: Response status code is " + response.getStatusCode());
+      log.info("Post Filter: Response status code is " + response.getStatusCode());
       // Add any custom logic here
     }));
   }
--- gateway/src/main/java/com/linkeleven/msa/gateway/CustomPreFilter.java
+++ gateway/src/main/java/com/linkeleven/msa/gateway/CustomPreFilter.java
@@ -1,23 +1,23 @@
 package com.linkeleven.msa.gateway;
 
-import java.util.logging.Logger;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
+
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
 @Component
+@Slf4j
 public class CustomPreFilter implements GlobalFilter, Ordered {
-
-  private static final Logger logger = Logger.getLogger(CustomPreFilter.class.getName());
 
   @Override
   public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
     ServerHttpRequest response = exchange.getRequest();
-    logger.info("Pre Filter: Request URI is " + response.getURI());
+    log.info("Pre Filter: Request URI is " + response.getURI());
     // Add any custom logic here
 
     return chain.filter(exchange);

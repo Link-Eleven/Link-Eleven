83e4215b75ec5f1468a22e8c80d5b93fb7588972
chc <311mikecho@gmail.com>
2025-01-03 14:04:46 +0900

    [ chore ] 불필요한 import 제거

--- feed/src/main/java/com/linkeleven/msa/feed/presentation/controller/external/FeedExternalController.java
+++ feed/src/main/java/com/linkeleven/msa/feed/presentation/controller/external/FeedExternalController.java
@@ -1,6 +1,5 @@
 package com.linkeleven.msa.feed.presentation.controller.external;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;

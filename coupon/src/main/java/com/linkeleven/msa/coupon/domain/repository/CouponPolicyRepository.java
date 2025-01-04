56433d44fbe64686ce817e20daa17aab8ba93538
geonuu09 <rjsdndl909@gmail.com>
2025-01-05 01:19:32 +0900

    [ refactor ] 스케쥴러 로직 수정 ( #57 )

--- coupon/src/main/java/com/linkeleven/msa/coupon/domain/repository/CouponPolicyRepository.java
+++ coupon/src/main/java/com/linkeleven/msa/coupon/domain/repository/CouponPolicyRepository.java
@@ -3,6 +3,7 @@ package com.linkeleven.msa.coupon.domain.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -11,4 +12,8 @@ import com.linkeleven.msa.coupon.domain.model.CouponPolicy;
 public interface CouponPolicyRepository extends JpaRepository<CouponPolicy, Long> {
 	@Query("SELECT p FROM CouponPolicy p WHERE p.couponId = :couponId AND p.issuedCount < p.quantity AND p.status = 'ACTIVE' ORDER BY p.createdAt ASC")
 	List<CouponPolicy> findAvailablePolicies(@Param("couponId") Long couponId);
+
+	@Query("UPDATE CouponPolicy p SET p.status = 'INACTIVE' WHERE p.couponId = :couponId AND p.status = 'ACTIVE'")
+	@Modifying
+	void updateCouponPolicyStatusToInactive(@Param("couponId") Long couponId);
 }
--- coupon/src/main/java/com/linkeleven/msa/coupon/domain/repository/CouponRepository.java
+++ coupon/src/main/java/com/linkeleven/msa/coupon/domain/repository/CouponRepository.java
@@ -1,10 +1,17 @@
 package com.linkeleven.msa.coupon.domain.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.linkeleven.msa.coupon.domain.model.Coupon;
 
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
 
 	boolean existsByFeedId(Long feedId);
+
+	@Query("SELECT c FROM Coupon c JOIN c.policies cp WHERE c.validTo < :currentTime AND cp.status <> 'INACTIVE'")
+	List<Coupon> findCouponsByValidToBeforeAndCouponPolicyStatusNot(LocalDateTime currentTime);
 }

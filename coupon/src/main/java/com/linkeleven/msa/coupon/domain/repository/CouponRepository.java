46bea42787fd94fec3aa1a3bd48e1245cf001326
geonuu09 <rjsdndl909@gmail.com>
2025-01-06 13:06:03 +0900

    [ refactor ] 사용하지 않는 코드 제거

--- coupon/src/main/java/com/linkeleven/msa/coupon/domain/repository/CouponRepository.java
+++ coupon/src/main/java/com/linkeleven/msa/coupon/domain/repository/CouponRepository.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import com.linkeleven.msa.coupon.domain.model.Coupon;
 import com.linkeleven.msa.coupon.infrastructure.repository.CouponRepositoryCustom;
@@ -18,14 +17,6 @@ public interface CouponRepository extends JpaRepository<Coupon, Long>, CouponRep
 	// 유효기한 지난 쿠폰조회
 	@Query("SELECT c FROM Coupon c JOIN c.policies cp WHERE c.validTo < :currentTime AND cp.status <> 'INACTIVE'")
 	List<Coupon> findCouponsByValidToBeforeAndCouponPolicyStatusNot(LocalDateTime currentTime);
-
-	// 발행된 쿠폰 수, 사용된 쿠폰 수 카운팅
-	@Query("SELECT c, " +
-		"(SELECT COUNT(ic) FROM IssuedCoupon ic WHERE ic.couponId = c.couponId) AS issuedCount, " +
-		"(SELECT COUNT(ic) FROM IssuedCoupon ic WHERE ic.couponId = c.couponId AND ic.status = 'USED') AS usedCount " +
-		"FROM Coupon c " +
-		"WHERE c.feedId = :feedId")
-	List<Object[]> findCouponsWithIssuedStatsByFeedId(@Param("feedId") Long feedId);
 
 	// feedId로 쿠폰 조회
 	Optional<Coupon> findByFeedId(Long feedId);

ff989e411d5e461e3d52ae9b05c2473e93a33d84
geonuu09 <rjsdndl909@gmail.com>
2025-01-09 00:47:38 +0900

    [ refactor ] 쿼리 메소드명 변경

--- coupon/src/main/java/com/linkeleven/msa/coupon/domain/repository/CouponRepository.java
+++ coupon/src/main/java/com/linkeleven/msa/coupon/domain/repository/CouponRepository.java
@@ -16,7 +16,7 @@ public interface CouponRepository extends JpaRepository<Coupon, Long>, CouponRep
 
 	// 유효기한 지난 쿠폰조회
 	@Query("SELECT c FROM Coupon c JOIN c.policies cp WHERE c.validTo < :currentTime AND cp.status <> 'INACTIVE'")
-	List<Coupon> findCouponsByValidToBeforeAndCouponPolicyStatusNot(LocalDateTime currentTime);
+	List<Coupon> findExpiredCoupons(LocalDateTime currentTime);
 
 	// feedId로 쿠폰 조회
 	Optional<Coupon> findByFeedId(Long feedId);

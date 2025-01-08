b1e4f97265683b875df0da5f55b9ac5777e908d3
dmlal <opkl8781@naver.com>
2025-01-08 19:12:04 +0900

    [ move ]  아웃박스 레포지토리 패키지 이동  #68

--- interaction/src/main/java/com/linkeleven/msa/interaction/domain/repository/OutboxRepository.java
+++
@@ -1,8 +0,0 @@
-package com.linkeleven.msa.interaction.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.linkeleven.msa.interaction.domain.model.entity.OutBox;
-
-public interface OutboxRepository extends JpaRepository<OutBox, Long> {
-}

7e07d9a2a589b323c8c7f3a1bf3061d40830ce76
jshstar <jshstar97@gmail.com>
2025-01-08 22:14:49 +0900

    [ feat ] 주소 조회 컨트롤러 기능 구현 ( #67 )

--- area/src/main/java/com/linkeleven/msa/area/presentation/controller/AreaQueryController.java
+++ area/src/main/java/com/linkeleven/msa/area/presentation/controller/AreaQueryController.java
@@ -2,16 +2,21 @@ package com.linkeleven.msa.area.presentation.controller;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.linkeleven.msa.area.application.dto.AreaSearchResponseDto;
 import com.linkeleven.msa.area.application.dto.LocationSearchResponseDto;
-import com.linkeleven.msa.area.application.service.AreaService;
+import com.linkeleven.msa.area.application.service.AreaQueryService;
 import com.linkeleven.msa.area.libs.dto.SuccessResponseDto;
 import com.linkeleven.msa.area.presentation.dto.LocationSearchRequestDto;
 
@@ -20,26 +25,36 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/areas")
-public class AreaController {
+public class AreaQueryController {
 
-	private final AreaService areaService;
+	private final AreaQueryService areaQueryService;
 
 	@PostMapping("/{areaId}/locations")
 	public ResponseEntity<SuccessResponseDto<List<LocationSearchResponseDto>>> searchLocationByRegion(
-		// @RequestHeader("X-User-Id") Long userId,
 		@PathVariable Long areaId,
 		@RequestBody LocationSearchRequestDto locationSearchRequestDto
-	){
+	) {
 
 		return ResponseEntity.status(HttpStatus.OK)
 			.body(
 				SuccessResponseDto.success(
 					"장소 정보 조회 및 생성 성공",
-					areaService.searchLocationByRegion(areaId, locationSearchRequestDto))
+					areaQueryService.searchLocationByRegion(areaId, locationSearchRequestDto))
 			);
 	}
 
-	// 조회 X
-
+	@GetMapping
+	public ResponseEntity<SuccessResponseDto<Page<AreaSearchResponseDto>>> searchAreaByKeyword(
+		@RequestParam String keyword,
+		Pageable pageable
+	) {
+		return ResponseEntity.status(HttpStatus.OK)
+			.body(
+				SuccessResponseDto.success(
+					"주소 정보 조회 성공",
+					areaQueryService.searchAreaByKeyword(keyword, pageable)
+				)
+			);
+	}
 
 }

65d0a60b720049b22bef1a51371a77c743bc9556
dmlal <opkl8781@naver.com>
2025-01-08 19:11:22 +0900

    [ feat ]  pending 이벤트 스케줄링으로 폴링 후 발행  #68

---
+++ interaction/src/main/java/com/linkeleven/msa/interaction/application/service/messaging/OutboxProcessorService.java
@@ -0,0 +1,41 @@
+package com.linkeleven.msa.interaction.application.service.messaging;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import org.springframework.kafka.support.SendResult;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import com.linkeleven.msa.interaction.domain.model.entity.OutBox;
+import com.linkeleven.msa.interaction.domain.model.enums.EventStatus;
+import com.linkeleven.msa.interaction.infrastructure.messaging.KafkaProducerService;
+import com.linkeleven.msa.interaction.infrastructure.repository.OutboxRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class OutboxProcessorService {
+
+	private final OutboxRepository outboxRepository;
+	private final KafkaProducerService kafkaProducerService;
+	private final OutboxStatusChangeService outboxStatusChangeService;
+
+	@Scheduled(fixedDelay = 7000)
+	public void sendPendingEvent() {
+		List<OutBox> pendingEventList = outboxRepository.findByEventStatus(EventStatus.PENDING);
+
+		for (OutBox outBox : pendingEventList) {
+			CompletableFuture<SendResult<String, Object>> future = kafkaProducerService.sendEvent(outBox);
+
+			future.whenComplete((result, throwable) -> {
+				if (throwable == null) {
+					outboxStatusChangeService.updateStatusProcessed(outBox);
+				} else {
+					outboxStatusChangeService.updateStatusFailed(outBox);
+				}
+			});
+		}
+	}
+}

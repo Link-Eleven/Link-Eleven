7c8368313f6657bdcd409e7cce5db4985f2327ae
chc <311mikecho@gmail.com>
2025-01-10 21:36:54 +0900

    [ refactor ] feignClient 관련 코드 추가

--- feed/src/main/java/com/linkeleven/msa/feed/infrastructure/client/AuthClient.java
+++ feed/src/main/java/com/linkeleven/msa/feed/infrastructure/client/AuthClient.java
@@ -4,12 +4,13 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import com.linkeleven.msa.feed.application.dto.external.UserInfoResponseDto;
+import com.linkeleven.msa.feed.application.dto.external.UserValidateIdResponseDto;
 
 @FeignClient(name = "auth-service")
 public interface AuthClient {
 
-	@GetMapping("/external/auth/{userId}")
-	UserInfoResponseDto getUsername(@PathVariable Long userId);
+	@GetMapping("/external/users/{userId}/validate")
+	UserValidateIdResponseDto getValidateUserId(@PathVariable("userId") Long userId);
+
 
 }

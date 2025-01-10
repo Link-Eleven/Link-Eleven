6172d3b1be8a3b9732bb090c83812ca3913bafd4
geonuu09 <rjsdndl909@gmail.com>
2025-01-10 10:23:50 +0900

    [ refactor ] Modifying 영속성을 고려한 코드로 수정

--- coupon/src/main/java/com/linkeleven/msa/coupon/domain/repository/CouponPolicyRepository.java
+++ coupon/src/main/java/com/linkeleven/msa/coupon/domain/repository/CouponPolicyRepository.java
@@ -15,12 +15,12 @@ public interface CouponPolicyRepository extends JpaRepository<CouponPolicy, Long
 	List<CouponPolicy> findAvailablePolicies(@Param("couponId") Long couponId);
 
 	// 스케쥴러: 유효기한 지난 쿠폰 비활성화
-	@Query("UPDATE CouponPolicy p SET p.status = 'INACTIVE' WHERE p.couponId = :couponId AND p.status = 'ACTIVE'")
-	@Modifying
-	void updateCouponPolicyStatusToInactive(@Param("couponId") Long couponId);
+	@Modifying(clearAutomatically = true)
+	@Query(
+		"UPDATE CouponPolicy p SET p.status = :status" + " WHERE p.couponId = :couponId " + "AND p.status = 'ACTIVE'")
+	void updateCouponPolicyStatusToInactive(@Param("couponId") Long couponId,
+		@Param("status") CouponPolicyStatus status);
 
-	// 파라미터인 쿠폰 상태 제외 후 조회
+	// 쿠폰 상태 제외 후 조회
 	List<CouponPolicy> findByCouponIdAndStatusNot(Long couponId, CouponPolicyStatus status);
-
-	void deleteByCouponId(Long couponId);
 }

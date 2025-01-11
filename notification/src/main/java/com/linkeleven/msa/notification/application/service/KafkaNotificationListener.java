61c83628b1acea81d88fe550b8d0933ba6bfa1d6
dmlal <opkl8781@naver.com>
2025-01-11 19:54:18 +0900

    [ refactor ]  payload 디코딩 설정 추가  #84

--- notification/src/main/java/com/linkeleven/msa/notification/application/service/KafkaNotificationListener.java
+++ notification/src/main/java/com/linkeleven/msa/notification/application/service/KafkaNotificationListener.java
@@ -2,8 +2,12 @@ package com.linkeleven.msa.notification.application.service;
 
 import java.io.IOException;
 
+import org.apache.avro.io.Decoder;
+import org.apache.avro.io.DecoderFactory;
+import org.apache.avro.specific.SpecificDatumReader;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Service;
 
@@ -33,15 +37,23 @@ public class KafkaNotificationListener {
 		}
 	)
 	@KafkaListener(topics = "comment_created", groupId = "notification-group")
-	public void listenCommentEvent(CommentEvent event) {
-		notificationService.saveNotificationEvent(
-			NotificationType.COMMENT,
-			event.getFeedId(),
-			event.getFeedAuthorId(),
-			event.getUserId(),
-			event.getUsername(),
-			event.getEventType()
-		);
+	public void listenCommentEvent(@Payload byte[] message) {
+		try {
+			SpecificDatumReader<CommentEvent> reader = new SpecificDatumReader<>(CommentEvent.class);
+			Decoder decoder = DecoderFactory.get().binaryDecoder(message, null);
+			CommentEvent event = reader.read(null, decoder);
+			notificationService.saveNotificationEvent(
+				NotificationType.COMMENT,
+				event.getFeedId(),
+				event.getFeedAuthorId(),
+				event.getUserId(),
+				event.getUsername(),
+				event.getEventType()
+			);
+			log.info("성공 : {}", event.getEventType());
+		} catch (IOException e) {
+			log.info("직렬화 실패 : {}", e.getMessage());
+		}
 	}
 
 	@RetryableTopic(
@@ -55,15 +67,23 @@ public class KafkaNotificationListener {
 		}
 	)
 	@KafkaListener(topics = "reply_created", groupId = "notification-group")
-	public void listenReplyEvent(ReplyEvent event) {
-		notificationService.saveNotificationEvent(
-			NotificationType.REPLY,
-			event.getCommentId(),
-			event.getCommentAuthorId(),
-			event.getUserId(),
-			event.getUsername(),
-			event.getEventType()
-		);
+	public void listenReplyEvent(@Payload byte[] message) {
+		try {
+			SpecificDatumReader<ReplyEvent> reader = new SpecificDatumReader<>(ReplyEvent.class);
+			Decoder decoder = DecoderFactory.get().binaryDecoder(message, null);
+			ReplyEvent event = reader.read(null, decoder);
+			notificationService.saveNotificationEvent(
+				NotificationType.REPLY,
+				event.getCommentId(),
+				event.getCommentAuthorId(),
+				event.getUserId(),
+				event.getUsername(),
+				event.getEventType()
+			);
+			log.info("성공 : {}", event.getEventType());
+		} catch (IOException e) {
+			log.info("직렬화 실패 : {}", e.getMessage());
+		}
 	}
 
 	@RetryableTopic(
@@ -76,16 +96,24 @@ public class KafkaNotificationListener {
 			IOException.class
 		}
 	)
-	@KafkaListener(topics = "like_created", groupId = "notification-group")
-	public void listenLikeEvent(LikeEvent event) {
-		notificationService.saveNotificationEvent(
-			NotificationType.LIKE,
-			event.getTargetId(),
-			event.getTargetAuthorId(),
-			event.getUserId(),
-			event.getUsername(),
-			event.getContentType()
-		);
+	@KafkaListener(topics = {"like_created_comment", "like_created_feed", "like_created_reply"}, groupId = "notification-group")
+	public void listenLikeEvent(@Payload byte[] message) {
+		try {
+			SpecificDatumReader<LikeEvent> reader = new SpecificDatumReader<>(LikeEvent.class);
+			Decoder decoder = DecoderFactory.get().binaryDecoder(message, null);
+			LikeEvent event = reader.read(null, decoder);
+			notificationService.saveNotificationEvent(
+				NotificationType.LIKE,
+				event.getTargetId(),
+				event.getTargetAuthorId(),
+				event.getUserId(),
+				event.getUsername(),
+				event.getContentType()
+			);
+			log.info("성공 : {}", event.getEventType());
+		} catch (IOException e) {
+			log.info("직렬화 실패 : {}", e.getMessage());
+		}
 	}
 
 	@KafkaListener(topics = "comment_created_dlt", groupId = "notification-group")

e6e76ac29ae5cb95ee91be27b0bbcf14c9e6c906
dmlal <opkl8781@naver.com>
2025-01-11 20:00:18 +0900

    [ rename ]  페인클라이언트 서비스명 변경  #84

--- feed/src/main/java/com/linkeleven/msa/feed/infrastructure/client/InteractionClient.java
+++ feed/src/main/java/com/linkeleven/msa/feed/infrastructure/client/InteractionClient.java
@@ -4,13 +4,12 @@ import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.linkeleven.msa.feed.application.dto.external.CommentCountResponseDto;
 import com.linkeleven.msa.feed.application.dto.external.LikeCountResponseDto;
 
-@FeignClient(name = "interaction")
+@FeignClient(name = "interaction-service")
 public interface InteractionClient {
 
 	@GetMapping("/external/feeds/comments")

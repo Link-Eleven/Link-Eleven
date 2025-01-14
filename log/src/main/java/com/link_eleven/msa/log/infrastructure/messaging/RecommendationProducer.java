f3be9c5735446581131602bb6f9395392c231011
geonuu09 <rjsdndl909@gmail.com>
2025-01-14 16:57:41 +0900

    [ refactor ] Kafka producer 코드 수정

--- log/src/main/java/com/link_eleven/msa/log/infrastructure/messaging/RecommendationProducer.java
+++ log/src/main/java/com/link_eleven/msa/log/infrastructure/messaging/RecommendationProducer.java
@@ -1,9 +1,6 @@
 package com.link_eleven.msa.log.infrastructure.messaging;
 
-import java.util.concurrent.CompletableFuture;
-
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
 
 import com.link_eleven.msa.log.domain.model.GeminiKeywordHolder;
@@ -18,7 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class RecommendationProducer {
 	private final KafkaTemplate<String, RecommendationMessage> kafkaTemplate;
-	private static final String TOPIC = "user-log-keywords";
+	private static final String TOPIC = "log.keywords";
 
 	public void sendToRecommendationService(GeminiKeywordHolder analysis) {
 		RecommendationMessage message = RecommendationMessage.builder()
@@ -26,17 +23,14 @@ public class RecommendationProducer {
 			.keywords(analysis.getKeywords())
 			.build();
 
-		log.info("RecommendationMessage 생성 완료 - userId: {}, keywords: {}",
-			message.getUserId(), message.getKeywords());
-
-		CompletableFuture<SendResult<String, RecommendationMessage>> future =
-			kafkaTemplate.send(TOPIC, message);
-
-		future.thenAccept(result ->
-			log.info("사용자에 대한 키워드를 성공적으로 보냈습니다.: {}", analysis.getUserId())
-		).exceptionally(ex -> {
-			log.error("키워드 전송에 실패했습니다", ex);
-			throw new CustomException(ErrorCode.KAFKA_SEND_FAILURE);
-		});
+		kafkaTemplate.send(TOPIC, message)
+			.whenComplete((result, ex) -> {
+				if (ex == null) {
+					log.info("사용자에 대한 키워드를 성공적으로 보냈습니다.: {}", analysis.getUserId());
+				} else {
+					log.error("키워드 전송에 실패했습니다", ex);
+					throw new CustomException(ErrorCode.KAFKA_SEND_FAILURE);
+				}
+			});
 	}
 }

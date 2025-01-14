67e47ef1de6cabbfb3e9ffdaaafe9ab61bd62629
geonuu09 <rjsdndl909@gmail.com>
2025-01-14 23:29:51 +0900

    [ refactor ] FeedLogService 수정 - 3개의 캐시가 쌓이면 제거하도록 코드 수정

--- recommendation/src/main/java/com/linkeleven/msa/recommendation/application/service/FeedLogService.java
+++ recommendation/src/main/java/com/linkeleven/msa/recommendation/application/service/FeedLogService.java
@@ -17,16 +17,32 @@ import lombok.RequiredArgsConstructor;
 public class FeedLogService {
 	private final FeedLogRepository feedLogRepository;
 	private final GeminiClient geminiClient;
+	private final CacheService cacheService;
 	private final LogToRecommendationServiceProducer logToRecommendationServiceProducer;
+	private static final String KEY_PREFIX = "feedlog:";
+	private static final int LOG_THRESHOLD = 3;
 
 	public void processNewLog(Long userId, String feedTitle) {
 		FeedLog feedLog = FeedLog.of(userId, feedTitle);
-		feedLogRepository.save(feedLog);
+		saveFeedLog(feedLog);
+		processRecommendationIfThresholdMet(userId);
+	}
 
+	private void saveFeedLog(FeedLog feedLog) {
+		feedLogRepository.save(feedLog);
+	}
+
+	private void processRecommendationIfThresholdMet(Long userId) {
 		List<FeedLog> latestLogs = feedLogRepository.getLatestLogs(userId);
-		if (latestLogs.size() >= 3) {
+		if (latestLogs.size() >= LOG_THRESHOLD) {
 			Recommendation analysis = geminiClient.processLogs(latestLogs);
 			logToRecommendationServiceProducer.sendToRecommendationService(analysis);
+			clearUserCache(userId);
 		}
+	}
+
+	private void clearUserCache(Long userId) {
+		String key = KEY_PREFIX + userId;
+		cacheService.delete(key);
 	}
 }

817fd5b15ecdf2afa5996a18ce6c730a839cd112
geonuu09 <rjsdndl909@gmail.com>
2025-01-15 15:40:45 +0900

    [ refactor ] 클래스명 변경으로 인한 수정

--- recommendation/src/main/java/com/linkeleven/msa/recommendation/application/service/FeedLogService.java
+++ recommendation/src/main/java/com/linkeleven/msa/recommendation/application/service/FeedLogService.java
@@ -8,7 +8,7 @@ import com.linkeleven.msa.recommendation.domain.model.FeedLog;
 import com.linkeleven.msa.recommendation.domain.model.Recommendation;
 import com.linkeleven.msa.recommendation.domain.repository.FeedLogRepository;
 import com.linkeleven.msa.recommendation.infrastructure.client.GeminiClient;
-import com.linkeleven.msa.recommendation.infrastructure.messaging.LogToRecommendationServiceProducer;
+import com.linkeleven.msa.recommendation.infrastructure.messaging.LogToRecommendationKafkaProducer;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,25 +18,29 @@ public class FeedLogService {
 	private final FeedLogRepository feedLogRepository;
 	private final GeminiClient geminiClient;
 	private final CacheService cacheService;
-	private final LogToRecommendationServiceProducer logToRecommendationServiceProducer;
+	private final LogToRecommendationKafkaProducer logToRecommendationKafkaProducer;
 	private static final String KEY_PREFIX = "feedlog:";
 	private static final int LOG_THRESHOLD = 3;
 
 	public void processNewLog(Long userId, String feedTitle) {
-		FeedLog feedLog = FeedLog.of(userId, feedTitle);
+		FeedLog feedLog = createFeedLog(userId, feedTitle);
 		saveFeedLog(feedLog);
-		processRecommendationIfThresholdMet(userId);
+		processRecommendationIfLogsSufficient(userId);
+	}
+
+	private FeedLog createFeedLog(Long userId, String feedTitle) {
+		return FeedLog.of(userId, feedTitle);
 	}
 
 	private void saveFeedLog(FeedLog feedLog) {
 		feedLogRepository.save(feedLog);
 	}
 
-	private void processRecommendationIfThresholdMet(Long userId) {
+	private void processRecommendationIfLogsSufficient(Long userId) {
 		List<FeedLog> latestLogs = feedLogRepository.getLatestLogs(userId);
 		if (latestLogs.size() >= LOG_THRESHOLD) {
 			Recommendation analysis = geminiClient.processLogs(latestLogs);
-			logToRecommendationServiceProducer.sendToRecommendationService(analysis);
+			logToRecommendationKafkaProducer.sendToRecommendationService(analysis);
 			clearUserCache(userId);
 		}
 	}

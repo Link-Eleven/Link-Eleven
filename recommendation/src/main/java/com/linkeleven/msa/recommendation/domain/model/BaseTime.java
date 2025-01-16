d7855c91dd3d8a72ac71b412ceb06a5b38cbf954
geonuu09 <rjsdndl909@gmail.com>
2025-01-16 17:07:59 +0900

    [ refactor ] JpaAuditorAware 제거 및 수동로직구현

--- recommendation/src/main/java/com/linkeleven/msa/recommendation/domain/model/BaseTime.java
+++ recommendation/src/main/java/com/linkeleven/msa/recommendation/domain/model/BaseTime.java
@@ -2,9 +2,7 @@ package com.linkeleven.msa.recommendation.domain.model;
 
 import java.time.LocalDateTime;
 
-import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -12,7 +10,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
 @MappedSuperclass
@@ -23,25 +20,7 @@ public abstract class BaseTime {
 	@Column(name = "created_at", updatable = false)
 	private LocalDateTime createdAt;
 
-	@Setter
-	@CreatedBy
-	@Column(name = "created_by", updatable = false)
-	private Long createdBy;
-
 	@LastModifiedDate
 	@Column(name = "updated_at")
 	private LocalDateTime updatedAt;
-
-	@Setter
-	@LastModifiedBy
-	@Column(name = "updated_by")
-	private Long updatedBy;
-
-	@Setter
-	@Column(name = "deleted_at")
-	private LocalDateTime deletedAt;
-
-	@Setter
-	@Column(name = "deleted_by")
-	private Long deletedBy;
 }
